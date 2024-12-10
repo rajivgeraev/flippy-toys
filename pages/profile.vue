9e1124de87c4b8fbd8be421020a2cf7d470d0cac
Rajiv Geraev <rajiv.geraev@gmail.com>
2024-12-10 05:07:31 +0300

    t2: new layout

---
+++ pages/profile.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="profile">
+      <h1>Profile</h1>
+      <p>Coming soon: Your profile information</p>
+    </div>
+  </template>
+  
+  <style scoped>
+  .profile {
+    padding: 1rem;
+    text-align: center;
+  }
+  </style>
