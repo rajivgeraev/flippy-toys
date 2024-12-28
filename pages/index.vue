57e8b5e2709d2a03cd1533229873e490f1bc48f6
Rajiv Geraev <rajiv.geraev@gmail.com>
2024-12-28 04:38:47 +0300

    t7: new sesign

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="p-4">
     <div class="grid grid-cols-2 gap-4">
-      <!-- <ToyCard v-for="toy in toys" :key="toy.id" :toy="toy" /> -->
+      <ToyCard v-for="toy in toys" :key="toy.id" :toy="toy" />
     </div>
   </div>
 </template>
 
 <script setup>
-// const { toys } = useToys()
+import ToyCard from '~/components/toys/ToyCard.vue';
+const { toys } = useToys()
 </script>
