3603b9347293699bb6c5730c0e5fb4046bdb0c80
Rajiv Geraev <rajiv.geraev@gmail.com>
2024-12-29 04:18:03 +0300

    t9: move to monorepo structure

--- pages/favorites.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="p-4">
-    <h2 class="text-xl font-semibold mb-4">Избранное</h2>
-    <p>Coming soon: Post your toys for exchange</p>
-    <!-- Добавьте содержимое страницы -->
-  </div>
-</template>
--- pages/messages.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="p-4">
-        <h2 class="text-xl font-semibold mb-4">Сообщения</h2>
-        <!-- Добавьте содержимое страницы -->
-    </div>
-</template>
